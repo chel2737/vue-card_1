b8f330ac73ffa9d0c22b69adde5f1c8d51bf4003
Vsevolod Anisimov <zuza226@mail.ru>
2022-12-10 14:47:03 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <UserCard/>
+  </div>
+</template>
+
+<script>
+import UserCard from './components/UserCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UserCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
